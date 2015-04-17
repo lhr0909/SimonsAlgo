0ae52233c110c6cb2a790ddcd143f57e02c71968
Liang <hlliang@amazon.com>
2015-04-17 11:35:55 -0700

    redid Round1A2014-A-small with XOR

--- _gcj/Round1A2014/A-ChargingChaos.py
+++ _gcj/Round1A2014/A-ChargingChaos.py
@@ -1,42 +1,28 @@
 from itertools import *
 
-def flip(outlet, switch):
-    #use XOR will make it faster
-    #return "{0:b}".format(int(outlet, 2) ^ )
-    outlet_list = list(outlet)
-    for index in switch:
-        if outlet_list[index] == '0':
-            outlet_list[index] = '1'
-        else:
-            outlet_list[index] = '0'
-    return ''.join(outlet_list)
-
 def solve(filename):
     fin = open(filename + '.in', 'r')
     fout = open(filename + '.out', 'w')
 
     T = int(fin.readline())
     for case in xrange(T):
-        answer = ""
+        answer = None
         #data read
         N, L = map(int, fin.readline().strip().split(" "))
         #print N, L
-        outlets = fin.readline().strip().split(" ")
-        devices = fin.readline().strip().split(" ")
-        #print outlets, devices
+        outlets = map(lambda x: int(x, 2), fin.readline().strip().split(" "))
+        devices = map(lambda x: int(x, 2), fin.readline().strip().split(" "))
+        # print outlets, devices
         if (frozenset(outlets) == frozenset(devices)):
             answer = 0
         else:
-            for i in xrange(L):
-                for indices in combinations(range(L), i):
-                    new_outlets = map(lambda x: flip(x, indices), outlets)
-                    #print indices, new_outlets, devices
-                    if (frozenset(new_outlets) == frozenset(devices)):
-                        answer = i
-                        break
-                if answer != "":
+            for flip_bits in xrange(1, 2**L):
+                new_outlets = map(lambda x: x ^ flip_bits, outlets)
+                #print indices, new_outlets, devices
+                if (frozenset(new_outlets) == frozenset(devices)):
+                    answer = list("{0:#b}".format(flip_bits)).count("1")
                     break
-        if answer == "":
+        if answer is None:
             answer = "NOT POSSIBLE"
         print answer
         fout.write(('Case #%d: ' % (case + 1)) + str(answer) + '\n')
@@ -45,4 +31,4 @@ def solve(filename):
     fout.close()
 
 if __name__ == "__main__":
-    solve("A-large")
+    solve("A-small-practice")
